82bdf6cb17a148c26b4b9bc9a7561c96271dc682
jenna0315 <jbellagamba@usf.edu>
2024-09-01 18:39:55 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -2,3 +2,11 @@ let employeeName = "Jerry" // string
 const employeeID = "12345678" // number
 var isActive = "true" // boolean
 console.log(employeeName,employeeID,isActive)
+
+// Array for list of products
+let order = ["laptop", "keyboard", "mouse"]
+// Object for product details
+const productdetails = {
+    name:"laptop",
+    price:"500",
+    inStock:"true"}
